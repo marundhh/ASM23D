e6c7b2e6a974df6c1e4c9eda2bdedec39ae15e8c
Khash <nlqkhanh16@gmail.com>
2025-02-28 22:18:08 +0700

    Swim

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿/*using Invector.vCharacterController;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Cần cài Layer của Player bơi thì nó mới có Animation
+//Lấy Vector 3 - tăng giảm y (theo chiều dọc)
+
+public class PlayerController : MonoBehaviour
+{
+    // Swim
+    public bool isSwimming;
+    public bool isUnderWater;
+
+    private float defaultExtraGravity;
+
+    private float currentSwimDepth = 3.95f; // Lưu trữ độ sâu hiện tại
+    private float maxSwimDepth = 3.95f; // biến độ sâu tối đa khi ở nước 
+
+    private Vector3 playerVelocity;
+
+    private void Start()
+    {
+        defaultExtraGravity = thirdPersonMotor.extraGravity;
+
+        playerVelocity = transform.position;
+    }
+
+    private void Update()
+    {
+        if (isSwimming)
+        {
+            if (isUnderWater)
+            {
+                Vector3 currentPosition = transform.position;
+
+                // Nhấn chuột phải để giảm vị trí y
+                if (Input.GetMouseButton(0)) // 1 là chuột phải
+                {
+                    currentSwimDepth = Mathf.Max(currentSwimDepth - 0.95f * Time.deltaTime, 0.3686f); // vd: 0.3686f chỉnh độ sâu nhất có thể lặn tới
+                }
+
+                // Nhấn chuột trái để tăng vị trí y
+                if (Input.GetMouseButton(1)) // 0 là chuột trái
+                {
+                    currentSwimDepth = Mathf.Min(currentSwimDepth + 0.95f * Time.deltaTime, maxSwimDepth);
+                }
+
+                // Cập nhật vị trí y theo currentSwimDepth
+                currentPosition.y = currentSwimDepth;
+
+                transform.position = currentPosition;
+            }
+            else
+            {
+                // Đặt vận tốc y về 0 khi bơi trên mặt nước
+                playerVelocity.y = 0;
+            }
+        }
+        else
+        {
+
+        }
+    }
+}*/
---
+++ Assets/Code/SwimArea.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+
+
+//bỏ  cái này vô code của nước
+/*public class SwimArea : MonoBehaviour
+{
+    public vThirdPersonController vThird;
+    public Transform cameraTransform;
+    public Animator animator;
+
+    void Start()
+    {
+        if (cameraTransform == null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            int swimAndDiveLayerIndex = animator.GetLayerIndex("Swim And Dive");
+            animator.SetLayerWeight(swimAndDiveLayerIndex, 1);
+            other.GetComponent<vThirdPersonController>().isSwimming = true;
+        }
+
+        if (other.CompareTag("MainCamera"))
+        {
+            other.GetComponentInParent<vThirdPersonController>().isUnderWater = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            int swimAndDiveLayerIndex = animator.GetLayerIndex("Swim And Dive");
+            animator.SetLayerWeight(swimAndDiveLayerIndex, 0);
+            other.GetComponent<vThirdPersonController>().isSwimming = false;
+        }
+
+        if (other.CompareTag("MainCamera"))
+        {
+            other.GetComponentInParent<vThirdPersonController>().isUnderWater = false;
+        }
+    }
+}*/
