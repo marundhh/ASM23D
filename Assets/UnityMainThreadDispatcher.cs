c2d7db03134b67975c41d3e63de4ef4913b09cde
ThMii <169111344+ThMii@users.noreply.github.com>
2025-03-02 18:19:08 +0700

    112

---
+++ Assets/UnityMainThreadDispatcher.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityMainThreadDispatcher : MonoBehaviour
+{
+    private static readonly Queue<Action> _executionQueue = new Queue<Action>();
+    private static UnityMainThreadDispatcher _instance;
+
+    public static UnityMainThreadDispatcher Instance()
+    {
+        if (_instance == null)
+        {
+            var obj = new GameObject("UnityMainThreadDispatcher");
+            _instance = obj.AddComponent<UnityMainThreadDispatcher>();
+            DontDestroyOnLoad(obj);
+        }
+        return _instance;
+    }
+
+    void Update()
+    {
+        lock (_executionQueue)
+        {
+            while (_executionQueue.Count > 0)
+            {
+                _executionQueue.Dequeue().Invoke();
+            }
+        }
+    }
+
+    public void Enqueue(IEnumerator action)
+    {
+        lock (_executionQueue)
+        {
+            _executionQueue.Enqueue(() => StartCoroutine(action));
+        }
+    }
+
+    public void Enqueue(Action action)
+    {
+        lock (_executionQueue)
+        {
+            _executionQueue.Enqueue(action);
+        }
+    }
+}
